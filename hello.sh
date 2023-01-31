d9aae2d14271d9809074e5c826a1026668b56384
obesavictory1 <obesavictory@gmail.com>
2023-01-31 09:45:34 -0500

    Test commit

---
+++ hello.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+    #Author : Utrains
+    #Date : 01-Nov-2021
+
+## ---------- script that display hello word in the screen -----------------
+
+echo "Yello Word !"
+pwd
+ls
+date
+whoami
